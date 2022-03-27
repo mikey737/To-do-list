62fe3fe7fd6523708eec779b706c9223673e35a5
Mikolaj Kaminski <mikolaj.kaminski737@gmail.com>
2022-03-28 00:56:59 +0200

    adding new tasks and showing if tasks are done

--- js/script.js
+++ js/script.js
@@ -1,30 +1,45 @@
 {
     const tasks = [
-        {
-            content: "wstać",
-            done: true,
-        },
-        {
-            content: "zjeść śniadanie",
-            done: false,
-        },
+        // {
+        //     content: "wstać",
+        //     done: true,
+        // },
+        // {
+        //     content: "zjeść śniadanie",
+        //     done: false,
+        // },
     ];
     const render = () => {
         let htmlString = "";
         for (const task of tasks) {
             htmlString += `
-                <li>
+                <li class="list__item${task.done ? " list__item--done":"" }">
                 ${task.content}
                 </li>
         `;
         }
         document.querySelector(".js-taskList").innerHTML = htmlString;
     }
+    const addNewTask = (newTaskContent) =>{
+        tasks.push({
+            content: newTaskContent,
+        }); 
+        render();
+    };
+    const onFormSubmit = ((event) =>{
+        event.preventDefault();
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        addNewTask(newTaskContent);
+        }) 
 
 
     const init = () => {
         render()
-    };
+        const form = document.querySelector(".js-form")
+        form.addEventListener("submit", onFormSubmit); 
+            render();
+
+        }
 
     init()
 }
